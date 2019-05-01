2aebb1462f98bdb03b9da6d92d0e56aa9b3a8b5e
Anton Hedström <antonhedstrom@gmail.com>
2019-05-01 12:56:35 +0200

    Skipping custom cors check and use env variable as origin.

--- cors-options.js
+++ cors-options.js
@@ -1,25 +1,12 @@
 require('dotenv').load();
 
-const whiteList = [
-  'https://weareonelaw.github.io',
-  process.env.CORS_ALLOW_ORIGIN,
-];
-
 module.exports = {
   all: {
     origin: '*',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   },
   protected: {
-    origin: (origin, callback) => {
-      if (!origin || whiteList.indexOf(origin) === -1) {
-        const error = new Error('Not allowed by CORS');
-        error.status = 400;
-        return callback(error, false);
-      }
-
-      return callback(null, true);
-    },
+    origin: process.env.CORS_ALLOW_ORIGIN,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   },
 }
